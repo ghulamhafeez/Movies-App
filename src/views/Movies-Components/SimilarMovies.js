ea26de58f982fa374c80e22e145000b87b46edee
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-02-08 22:52:00 +0500

    Fixed minor issues

--- src/views/Movies-Components/SimilarMovies.js
+++ src/views/Movies-Components/SimilarMovies.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Grid } from "@mui/material";
-import { useNavigate ,useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/navigation";
 import Typography from "@mui/material/Typography";
 import { useGetSimilarMoviesQuery } from "../../services/MovieServices";
-import {MovieSwiper} from "./Swiper"
+import { MovieSwiper } from "./Swiper";
 
 export const SimilarMovies = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const { data =[] } = useGetSimilarMoviesQuery(id);
-  console.log("dataS",data)
-  const handleRedirect = ({id}) => {
+  const { data = [] } = useGetSimilarMoviesQuery(id);
 
+  const handleRedirect = ({ id }) => {
     navigate(`/movies-detail/${id}`);
   };
 
@@ -30,7 +29,7 @@ export const SimilarMovies = () => {
       >
         Similar Movies
       </Typography>
-      <MovieSwiper  data={data}  handleRedirect={(x)=>handleRedirect(x)}/>
+      <MovieSwiper data={data} handleRedirect={(x) => handleRedirect(x)} />
     </Grid>
   );
 };
